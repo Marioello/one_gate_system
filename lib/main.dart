e0b7ddc34c5f8440d24b1a4368e3125a6226d240
Romario Putinela <toshiro89@gmail.com>
2023-06-27 17:05:11 +0700

    rp - homepage - Add new layout for web

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class MyApp extends StatelessWidget {
       value: AuthService().user,
       initialData: null,
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         scrollBehavior: ScrollConfiguration.of(context).copyWith(
           dragDevices: {
             PointerDeviceKind.touch,
